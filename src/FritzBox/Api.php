eb551acab066f07dace16474c1e285409af1fa15
Roman Martin <roman.martin@uni-marburg.de>
2019-01-06 21:33:00 +0100

    Disable certificate verification

--- src/FritzBox/Api.php
+++ src/FritzBox/Api.php
@@ -55,6 +55,8 @@ class Api
             curl_setopt($ch, CURLOPT_URL, $this->url . '/cgi-bin/webcm');
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($formfields));
         $output = curl_exec($ch);
@@ -75,6 +77,8 @@ class Api
         }
         curl_setopt($ch, CURLOPT_URL, $this->url . '/cgi-bin/firmwarecfg');
         curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
         // enable for debugging:
         //curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
@@ -188,7 +192,11 @@ class Api
 
         $url = $this->url . $getpage . http_build_query($params);
 
-        $this->client = $this->client ?? new Client();
+        $this->client = $this->client ?? new Client(
+	 array(
+	  'verify' => false
+	  )
+        );
         $response = $this->client->send(new Request('GET', $url));
 
         if (200 !== $response->getStatusCode()) {
